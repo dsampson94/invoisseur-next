d800b390a58b32c4393690724fb29bedf2640b3f
dsampson-cs <david.sampson@cloudsmiths.global>
2024-09-23 16:31:34 +0200

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,33 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import GoogleAnalytics from '@/app/components/GoogleAnalytics';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Invoisseur Free Invoice Maker",
-  description: "Invoisseur Free Invoice Maker",
+    title: 'Invoisseur Free Invoice Maker',
+    description: 'Invoisseur Free Invoice Maker',
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-      <html lang="en">
-      <head>
-        {/* Google AdSense Code */ }
-        <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5775380070471697"
-            crossOrigin="anonymous"
-        ></script>
-        <title>Invoisseur Free Invoice Maker</title>
-      </head>
-      <body className={ inter.className }>{ children }</body>
-      </html>
-  );
+    return (
+        <html lang="en">
+        <head>
+            {/* Google AdSense Code */ }
+            <script
+                async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5775380070471697"
+                crossOrigin="anonymous"
+            ></script>
+            <title>Invoisseur Free Invoice Maker</title>
+            <GoogleAnalytics/>
+        </head>
+        <body className={ inter.className }>{ children }</body>
+        </html>
+    );
 }
